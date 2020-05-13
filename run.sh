633edc01974418675736dcf93b94bb9ec741abd4
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-05-13 13:48:11 +0000

    run.sh: user root is not used anymore

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ image="${2:-maurelio1234/emacs-docker}"
 if [[ "$mode" == "text" ]]; then
     docker run -it \
            -e "TERM=xterm-256color" \
-           -v /home/$USER/.ssh:/root/.ssh \
+           -v /home/$USER/.ssh:/home/$USER/.ssh \
            -v /mnt/data/bitbucket:/home/$USER/bitbucket \
            -v /mnt/data/github:/home/$USER/github \
            -v /mnt/data/exercism:/home/$USER/exercism \
@@ -20,7 +20,7 @@ else
     docker run -it \
            -e DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v /home/$USER/.ssh:/root/.ssh \
+           -v /home/$USER/.ssh:/home/$USER/.ssh \
            -v /mnt/data/bitbucket:/home/$USER/bitbucket \
            -v /mnt/data/github:/home/$USER/github \
            -v /mnt/data/exercism:/home/$USER/exercism \
