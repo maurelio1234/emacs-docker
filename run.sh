f66d25d5d70249a66f18ad434e8f1637a10d7436
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-05-13 14:04:53 +0000

    refacto: run.sh

--- run.sh
+++ run.sh
@@ -2,30 +2,30 @@
 
 mode="$1"
 image="${2:-maurelio1234/emacs-docker}"
+volumes=" -v /home/$USER/.ssh:/home/$USER/.ssh \
+          -v /mnt/data/bitbucket:/home/$USER/bitbucket \
+          -v /mnt/data/github:/home/$USER/github \
+          -v /mnt/data/exercism:/home/$USER/exercism \
+          -v /var/run/docker.sock:/var/run/docker.sock"
+common_parametes=" --rm \
+                   --net=host \
+                   --name emacs"
 
-if [[ "$mode" == "text" ]]; then
-    docker run -it \
-           -e "TERM=xterm-256color" \
-           -v /home/$USER/.ssh:/home/$USER/.ssh \
-           -v /mnt/data/bitbucket:/home/$USER/bitbucket \
-           -v /mnt/data/github:/home/$USER/github \
-           -v /mnt/data/exercism:/home/$USER/exercism \
-           -v /var/run/docker.sock:/var/run/docker.sock \
-           --net=host \
-           --name emacs \
-           maurelio1234/emacs-docker
-elif [[ "$mode" == "build" ]]; then
+if [[ "$mode" == "build" ]]; then
     docker build -t "$image" .
-else
-    docker run -it \
+elif [[ "$mode" == "text" ]]; then
+    docker run $common_parametes \
+           --interactive \
+           --tty \
+           -e "TERM=xterm-256color" \
+           $volumes \
+           "$image"
+elif [[ "$mode" == "gui" ]]; then
+    docker run $common_parametes \
            -e DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v /home/$USER/.ssh:/home/$USER/.ssh \
-           -v /mnt/data/bitbucket:/home/$USER/bitbucket \
-           -v /mnt/data/github:/home/$USER/github \
-           -v /mnt/data/exercism:/home/$USER/exercism \
-           -v /var/run/docker.sock:/var/run/docker.sock \
-           --net=host \
-           --name emacs \
+           $volumes \
            "$image"
+else
+    echo "usage: $0 [build|text|gui] [image]"
 fi
